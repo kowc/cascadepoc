077c732694316feb436f72f3406f566435a13dec
jkasivis <jkasivis@DEVINSTSIN1>
2017-09-11 11:16:06 +0000

    Add project files.

---
+++ gitconnection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitconnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           String st= Console.ReadLine().ToString();
+            Console.WriteLine(st);
+            Console.ReadKey();
+        }
+    }
+}
