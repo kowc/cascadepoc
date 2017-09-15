4e890edd47c21c27af1b23f5903995179108cf1f
kpalani <simplesimple0409@gmail.com>
2017-09-15 11:46:11 +0530

    creating first change on 15-09

---
+++ gitconnection/newk1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitconnection
+{
+    class newk1
+    {
+        static void Main(string[] args)
+        {
+           String st= Console.ReadLine().ToString();
+            Console.WriteLine(st);
+            Console.WriteLine(st.ToUpper());//changes
+            Console.WriteLine("Converted string to upper case");
+            Console.ReadKey();
+        }
+    }
+}
